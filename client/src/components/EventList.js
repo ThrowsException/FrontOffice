591d9e6ce26a9741b979d894f601b599d203f2dd
Chester ONeill <coneillcodes@gmail.com>
2019-07-14 19:06:32 -0400

    Adding sick animations

--- client/src/components/EventList.js
+++ client/src/components/EventList.js
@@ -1,21 +1,65 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { motion } from "framer-motion";
+import styled from "styled-components";
+import List from "@material-ui/core/List";
+
+const list = {
+  show: {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.1
+    }
+  },
+  hidden: {
+    opacity: 0
+  }
+};
+
+const items = {
+  show: { opacity: 1, x: 0 },
+  hidden: { opacity: 0, x: -100 }
+};
+
+const Events = styled(motion.div)`
+  width: 400px;
+  list-style: none;
+`;
+
+const Event = styled(motion.div)`
+  max-width: 100px;
+`;
+
+const EventTitle = styled.h2`
+  font-weight: 300;
+  text-transform: capitalize;
+`;
 
 const EventList = ({ events }) => {
   return (
     <>
       {events.length > 0 ? (
-        <ul>
-          {events.map(events => (
-            <li key={events.id}>
-              {events.name} {events.email}
-            </li>
+        <Events animate="show" initial="hidden" variants={list}>
+          {events.map((events, i) => (
+            <Event key={events.id} variants={items}>
+              <EventTitle>{events.name}</EventTitle> {events.email}
+            </Event>
           ))}
-        </ul>
+        </Events>
       ) : (
-        <span>No Upcoming Events</span>
+        <div>No Upcoming Events</div>
       )}
     </>
   );
+};
+
+EventList.propTypes = {
+  events: PropTypes.array
+};
+
+EventList.defaultProps = {
+  events: []
 };
 
 export default EventList;
