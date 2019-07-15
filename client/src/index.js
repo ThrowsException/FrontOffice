81390701dcf02af4d8d1800e40f69ba575681e5e
Chester ONeill <coneillcodes@gmail.com>
2019-07-14 20:27:48 -0400

    Im too lazy to figure out browser routing right now

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { MemoryRouter as Router, Route, Switch } from "react-router-dom";
 import { TeamDetails, Login } from "./components";
 import Teams from "./pages/Teams";
 
@@ -9,16 +9,11 @@ import "typeface-roboto";
 function AppRouter() {
   return (
     <Router>
-      <div style={{ display: "flex" }}>
-        <main style={{ flexGrow: 1 }}>
-          <Switch>
-            <Route path="" exact component={Login} />
-            <Route path="/" exact component={Login} />
-            <Route path="/teams" exact component={Teams} />
-            <Route path="/teams/:id" component={TeamDetails} />
-          </Switch>
-        </main>
-      </div>
+      <Switch>
+        <Route path="/teams" exact component={Teams} />
+        <Route path="/teams/:id" component={TeamDetails} />
+        <Route component={Login} />
+      </Switch>
     </Router>
   );
 }
